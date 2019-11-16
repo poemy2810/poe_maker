7044da6a7a1ea51c25034e6bd03aa0949fcd25d8
poemy_Okamoto <57334338+poemy2810@users.noreply.github.com>
2019-11-16 22:41:36 +0900

    配列を使って、画像をランダムに表示させたい。

--- app/src/main/java/app/okamoto/poemy/makeitpoemy/MainActivity.kt
+++ app/src/main/java/app/okamoto/poemy/makeitpoemy/MainActivity.kt
@@ -2,18 +2,45 @@ package app.okamoto.poemy.makeitpoemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
+    val summerList = arrayOf("海", "かき氷", "夏休み", "暑い", "")
+
+    val summerView = arrayOf("summer1", "summer2", "summer3", "summer4", "summer5")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appdataButton.setOnClickListener {
+
+            val randomnum = Random.nextInt(summerList.size)
+            textView.text = summerList[randomnum]
+
+            val randomnumber = Random.nextInt(summerView.size)
+
+        }
+
+
+
+        /*override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            appdataButton.setOnClickListener {
+
+
+        }
+            */
+
+
     }
-
-    //textStart.setOnClickListen
-
-    //}
-
-
-
 }
+
+
+
